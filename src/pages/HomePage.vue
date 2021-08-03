3ea7d130e653b6db97bf3b527749741b81886516
cjgreenie <seejarom@gmail.com>
2021-08-03 12:52:53 -0600

    setup

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="component">
+
+
+  </div>
+</template>
+
+
+<script>
+import { reactive } from "@vue/reactivity"
+
+export default {
+  name: 'HomePage',
+    setup(){
+      const state = reactive({
+
+      })
+    return { state }
+  },
+  components:{}
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
